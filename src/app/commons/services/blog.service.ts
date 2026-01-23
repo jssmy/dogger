f592e597f83d03f87a03db4eefebe63dbad10920
Joset Manihuari <jssmy@Josets-MacBook-Air.local>
2026-01-23 11:01:16 -0500

    adding new endpoint for restrcit evaluation

--- src/app/commons/services/blog.service.ts
+++ src/app/commons/services/blog.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { map, mergeMap, of } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Blog, BlogStage } from '../interfaces/blog';
-import { map, mergeMap, of } from 'rxjs';
 import { BlogWriter } from '../interfaces/blog-writer';
 
 @Injectable({
@@ -13,18 +13,18 @@ export class BlogService {
 
   getPublicBlog(slug: string) {
     return this.http.get<Blog>(`${environment.blog}/public/${slug}`)
-    .pipe(
-      mergeMap(
-        blog => {
-          return this.getBlogWriter(blog.userId)
-          .pipe(
-            mergeMap(writer => of({
-              blog,
-              writer
-            }))
-          )
-        }
-      ));
+      .pipe(
+        mergeMap(
+          blog => {
+            return this.getBlogWriter(blog.userId)
+              .pipe(
+                mergeMap(writer => of({
+                  blog,
+                  writer
+                }))
+              )
+          }
+        ));
   }
 
   getBlogWriter(userId: string) {
@@ -36,10 +36,10 @@ export class BlogService {
   }
 
   create(blog: Blog) {
-    return this.http.post<Blog>(`${environment.blog}`, blog);
+    return this.http.post<Blog>(`${environment.blog}/create`, blog);
   }
 
-  update (blog: Partial<Blog>, slug: string) {
+  update(blog: Partial<Blog>, slug: string) {
     return this.http.put(`${environment.blog}/${slug}`, blog);
   }
 
@@ -62,14 +62,14 @@ export class BlogService {
 
   getBlogs() {
     return this.http.get<Blog[]>(`${environment.blog}/own`)
-    .pipe(
-      map(blogs => blogs.map(blog => ({
-        ...blog,
-        title: blog.blocks.find(block => block.type === 'header' )?.data.text
-      })))
-    );
+      .pipe(
+        map(blogs => blogs.map(blog => ({
+          ...blog,
+          title: blog.blocks.find(block => block.type === 'header')?.data.text
+        })))
+      );
   }
 
-  
+
 
 }
